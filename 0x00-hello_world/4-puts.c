6988d3b68f38e50cf6b8445c4a01dc3b36c2bee0
amxsupport <itmasterdesigne@gmail.com>
2023-06-15 10:21:03 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void)
+{
+	/* this is simple code that type a puts string */
+	char mystr[] = '"Programming is like building a multilingual puzzle"';
+	puts(mystr);
+
+	return (0);
+}
