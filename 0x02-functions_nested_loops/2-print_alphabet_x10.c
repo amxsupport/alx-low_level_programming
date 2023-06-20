02b50be889df248d76d202c976c96b1008121399
amxsupport <itmasterdesigne@gmail.com>
2023-06-20 19:55:44 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet x 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char lettre;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (lettre = 'a'; lettre <= 'z'; lettre++)
+			_putchar(lettre);
+	}
+	_putchar('\n');
+}
