4a6ad14621e4baa6d7255b6bf97f08e349a1e62c
amxsupport <itmasterdesigne@gmail.com>
2023-06-22 19:19:00 +0000

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: itmasterdesigne
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x04-more_functions_nested_loops directory.
+ */
+
+int _isupper(int c);
+
+#endif
