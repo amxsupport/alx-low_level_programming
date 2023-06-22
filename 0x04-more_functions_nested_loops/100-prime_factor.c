42da986fe93ed9b351ae74da708f39da8e617db8
amxsupport <itmasterdesigne@gmail.com>
2023-06-22 22:50:50 +0000

    update 100-prime_factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,12 +12,13 @@
 int main(void)
 {
 	long n = 612852475143;
+	int i;
 
 	while (n % 2 == 0)
 	{
 		n = n / 2;
 	}
-	for (int i = 3; i <= sqrt(n); i += 2)
+	for (i = 3; i <= sqrt(n); i += 2)
 	{
 		while (n % i == 0)
 		{
