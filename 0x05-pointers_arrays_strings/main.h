3bbcffec30c6d5964e19497e8f00584c68fb6636
amxsupport <itmasterdesigne@gmail.com>
2023-06-27 00:01:25 +0000

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: holberton.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x05-pointers_arrays_strings directory.
+ */
+
+void reset_to_98(int *n);
+
+#endif
