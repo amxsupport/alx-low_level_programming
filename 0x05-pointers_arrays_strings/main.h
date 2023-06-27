d2329ae34710f54a161a797854520d388205839f
amxsupport <itmasterdesigne@gmail.com>
2023-06-27 00:33:35 +0000

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,8 @@
  *       used in the 0x05-pointers_arrays_strings directory.
  */
 
+#include <stdlib.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 size_t _strlen(const char *str);
