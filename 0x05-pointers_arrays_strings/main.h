4baaa97cf7e82f02022248c89de912901741404f
amxsupport <itmasterdesigne@gmail.com>
2023-06-27 01:06:17 +0000

    6 7 8 9

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -18,5 +18,10 @@ size_t _strlen(const char *str);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
 
 #endif
